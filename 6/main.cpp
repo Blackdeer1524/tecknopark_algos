37d281983ffae284727c566abdd7a6a6c32cf047
Danila Slesarev <blackdeer1524@yandex.ru>
2023-03-30 12:51:30 +0300

    6 wip

---
+++ 6/main.cpp
@@ -0,0 +1,84 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <optional>
+#include <utility>
+
+template <typename T>
+auto partition(T       *array,
+               uint64_t l,
+               uint64_t r,
+               auto(*cmp)(const T &left, const T &right)->int) -> uint64_t {
+    auto     pivot = array[r];
+    uint64_t j     = l - 1;
+    for (uint64_t i = l; i <= r - 1; ++i) {
+        if (cmp(array[i], pivot) <= 0) {
+            std::swap(array[++j], array[i]);
+        }
+    }
+    std::swap(array[++j], array[r]);
+    return j;
+}
+
+template <typename T>
+auto random_partition(T       *array,
+                      uint64_t l,
+                      uint64_t r,
+                      auto(*cmp)(const T &left, const T &right)->int)
+    -> uint64_t {
+    auto     subarray_length    = r - l + 1;
+    uint64_t random_pivot_index = rand() % subarray_length;
+    std::swap(array[r], array[l + random_pivot_index]);
+    return partition(array, l, r, cmp);
+}
+
+template <typename T>
+auto get_nth_order_statistic(T       *array,
+                             uint64_t length,
+                             uint64_t order_statistic,
+                             auto(*cmp)(const T &left, const T &right)->int)
+    -> T {
+    uint64_t l = 0;
+    uint64_t r = length - 1;
+    while (l < r) {
+        auto q = random_partition(array, l, r, cmp);
+        auto k = q - l + 1;
+        if (k - 1 == order_statistic) {
+            return array[q];
+        } else if (k > order_statistic) {
+            r = q - 1;
+        } else {
+            order_statistic -= k;
+            l = q + 1;
+        }
+    }
+    return array[l];
+}
+
+auto main() -> int {
+    auto cmp = [](const int &left, const int &right) -> int {
+        if (left == right) {
+            return 0;
+        } else if (left < right) {
+            return -1;
+        } else {
+            return 1;
+        }
+    };
+    uint64_t length;
+    std::cin >> length;
+    int *array = new int[length];
+    for (uint64_t i = 0; i < length; i++) {
+        std::cin >> array[i];
+    }
+
+    uint64_t tenth      = length / 10;
+    uint64_t nineteenth = length - tenth;
+    uint64_t median     = length / 2;
+    std::cout << get_nth_order_statistic<int>(array, length, tenth, cmp)
+              << std::endl
+              << get_nth_order_statistic<int>(array, length, median, cmp)
+              << std::endl
+              << get_nth_order_statistic<int>(array, length, nineteenth, cmp);
+    delete[] array;
+}
