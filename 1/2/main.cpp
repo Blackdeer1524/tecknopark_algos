45673d4f29f1aa9867c6c170ebd68f73fc5ef42c
Danila Slesarev <blackdeer1524@yandex.ru>
2023-05-02 14:44:11 +0300

    started moving exercises to separate branches

--- 1/2/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    uint32_t number, query;
-    std::cin >> number >> query;
-    auto result = (number >> query) & 1;
-    std::cout << result;
-}
--- 1/4/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdint>
-#include <iostream>
-
-int main() {
-    uint32_t number, index;
-    std::cin >> number >> index;
-    auto result = number ^ (1 << index);
-    std::cout << result;
-}
