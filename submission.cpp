84d79949f6b48d0a02f7088717e5e2ad44b88c7b
Danila Slesarev <blackdeer1524@yandex.ru>
2023-05-06 22:16:04 +0300

    DONE

--- submission.cpp
+++ submission.cpp
@@ -1,4 +1,79 @@
-#include "hashmap.hpp"
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <cstdint>
+#include <functional>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+// Вариант 2
+// Для разрешения коллизий используйте двойное хеширование.
+
+class HashMap {
+ public:
+    using hash_function_t = std::function<uint64_t(std::string)>;
+
+    HashMap(hash_function_t main_hash_function,
+            hash_function_t resolver_hash_function);
+
+    HashMap(const HashMap &)                     = delete;
+    HashMap(HashMap &&)                          = default;
+    auto operator=(const HashMap &) -> HashMap & = delete;
+    auto operator=(HashMap &&) -> HashMap      & = default;
+
+    auto insert(std::string &&item) -> bool;
+
+    auto contains(const std::string &key) -> bool;
+
+    auto remove(const std::string &item) -> bool;
+
+    auto print() -> void;
+
+ private:
+    struct Item {
+        explicit Item(std::optional<std::string> &&value);
+
+        Item()                                 = default;
+        Item(const Item &)                     = default;
+        Item(Item &&)                          = default;
+        auto operator=(const Item &) -> Item & = default;
+        auto operator=(Item &&) -> Item      & = default;
+
+        auto get_value_opt() const -> const std::optional<std::string> &;
+
+        auto is_removed() const -> bool;
+
+        auto remove() -> void;
+
+     private:
+        std::optional<std::string> value_;
+        bool                       is_removed_;
+    };
+
+    using storage_t                             = std::vector<Item>;
+
+    static constexpr int    HASHTABLE_INIT_SIZE = 8;
+    static constexpr double GROW_FRACTION       = 0.75;
+    hash_function_t         main_hash_function_;
+    hash_function_t         resolver_hash_function_;
+    storage_t               storage_;
+    std::vector<bool>       deleted_;
+    uint64_t                size_ = 0;
+
+    auto                    resize() -> void;
+
+    auto get_insertion_index(const std::string &item, const storage_t &src)
+        -> bool;
+
+    auto insert_with_resolution(std::string &&item, storage_t &dst) -> bool;
+
+    auto find_item(const std::string &item, const storage_t &src)
+        -> std::pair<bool, uint64_t>;
+};
+
+#endif  // !HASHMAP_H
 
 #include <algorithm>
 #include <cmath>
@@ -34,7 +109,6 @@ auto HashMap::Item::remove() -> void {
     is_removed_ = true;
 }
 
-// all right
 auto HashMap::find_item(const std::string &item, const storage_t &src)
     -> std::pair<bool, uint64_t> {
     uint64_t insertion_index = main_hash_function_(item) % src.capacity();
@@ -141,4 +215,73 @@ auto HashMap::print() -> void {
         std::cout << std::setw(4) << item.is_removed() << " ";
     }
     std::cout << "}";
+}
+
+#include <cstdint>
+#include <iostream>
+#include <numeric>
+
+auto hash1(const std::string &str) -> uint64_t {
+    static uint64_t a   = 13;
+    uint64_t        res = 0;
+    for (unsigned char c : str) {
+        res = res * a + c;
+    }
+    // return 0zu;
+    return res;
+}
+
+auto hash2(const std::string &str) -> uint64_t {
+    uint64_t res = 0;
+    for (unsigned char c : str) {
+        res = res + c;
+    }
+    // return 1;
+    return 2 * res + 1;
+}
+
+auto main() -> int {
+    auto map = HashMap(hash1, hash2);
+    while (std::cin) {
+        char op;
+        std::cin >> op;
+        if (!std::cin) {
+            break;
+        }
+        std::string t;
+        std::cin >> t;
+        switch (op) {
+            case '+': {
+                auto flag = map.insert(std::move(t));
+                if (flag) {
+                    std::cout << "FAIL" << std::endl;
+                } else {
+                    std::cout << "OK" << std::endl;
+                }
+                // map.print();
+                // std::cout << std::endl;
+            } break;
+            case '?': {
+                auto flag = map.contains(t);
+                if (flag) {
+                    std::cout << "OK" << std::endl;
+                } else {
+                    std::cout << "FAIL" << std::endl;
+                }
+            } break;
+            case '-': {
+                auto flag = map.remove(t);
+                if (flag) {
+                    std::cout << "FAIL" << std::endl;
+                } else {
+                    std::cout << "OK" << std::endl;
+                }
+                // map.print();
+                // std::cout << std::endl;
+            } break;
+            default:
+                std::cout << "Unknown operation" << std::endl;
+                break;
+        }
+    }
 }
