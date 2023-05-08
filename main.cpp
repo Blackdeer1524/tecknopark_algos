76e8c2fc035b44511563928f279f98f3776c202e
Danila Slesarev <blackdeer1524@yandex.ru>
2023-05-08 21:27:37 +0300

    user input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "btree.hpp"
+#include <cstdint>
 #include <ostream>
 
 auto main() -> int {
-    auto tree = BTree::build(2, [](int &left, int &right) -> int {
+    uint64_t t;
+    std::cin >> t;
+
+    auto tree = BTree::build(t, [](int &left, int &right) -> int {
         if (left < right) {
             return -1;
         }
@@ -11,69 +15,13 @@ auto main() -> int {
         }
         return 0;
     });
-
-    // tree->insert(7);
-    // tree->print();
-    // std::cout << std::endl << std::endl;
-
-    // tree->insert(5);
-    // tree->print();
-    // std::cout << std::endl << std::endl;
-
-    // tree->insert(0);
-    // tree->print();
-    // std::cout << std::endl << std::endl;
-
-    // tree->insert(5);
-    // tree->print();
-    // std::cout << std::endl << std::endl;
-
-    // tree->insert(9);
-    // tree->insert(9);
-    // tree->insert(9);
-    // tree->insert(9);
-    // tree->print();
-    // std::cout << std::endl << std::endl;
-
-    // 2
-    // 0 1 2 3 4 5 6 7 8 9
-
-    tree->insert(0);
+    for (;;) {
+        int value;
+        std::cin >> value;
+        if (!std::cin) {
+            break;
+        }
+        tree->insert(std::move(value));
+    }
     tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(1);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(2);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(3);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(4);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(5);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(6);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(7);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(8);
-    tree->print();
-    std::cout << std::endl << std::endl;
-
-    tree->insert(9);
-    tree->print();
-    std::cout << std::endl << std::endl; }
+}
